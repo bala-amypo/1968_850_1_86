1e27f97b94d414a34d7eeac9b3754e47cef2f33d
bala-amypo <bala@amypo.in>
2025-12-22 05:51:23 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -3,22 +3,20 @@ package com.example.demo.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
+    
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Crop & Fertilizer Suggestion API")
                         .version("1.0")
-                        .description("API for crop and fertilizer suggestions"))
-                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                        .description("API for managing farms, crops, fertilizers and suggestions"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme()
