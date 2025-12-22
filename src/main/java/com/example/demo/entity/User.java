6511f423906409407d3b6613578297e3a6f40c46
bala-amypo <bala@amypo.in>
2025-12-22 13:01:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,12 +1,14 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder; // <--- Add this import
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder // <--- Add this annotation
 public class User {
     private Long id;
     private String email;
