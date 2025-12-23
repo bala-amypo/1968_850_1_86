45a1962ae071790875d1839cb5ac30c37ab0dea2
bala-amypo <bala@amypo.in>
2025-12-23 03:53:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/CropRequest.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+public class CropRequest {
+    private String name;
+    private String season;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+}
