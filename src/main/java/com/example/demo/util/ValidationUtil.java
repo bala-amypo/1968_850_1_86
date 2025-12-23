a83292385c70206ff676f53c096111b17d55d585
bala-amypo <bala@amypo.in>
2025-12-23 04:15:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/ValidationUtil.java
@@ -0,0 +1,12 @@
+package com.example.demo.util;
+
+import java.util.Set;
+
+public class ValidationUtil {
+
+    private static final Set<String> SEASONS = Set.of("Kharif", "Rabi", "Summer");
+
+    public static boolean validSeason(String season) {
+        return SEASONS.contains(season);
+    }
+}
