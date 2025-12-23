56c214472479b1c1fe6f94ebea6dab8002178a5e
bala-amypo <bala@amypo.in>
2025-12-23 04:28:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/SuggestionController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Suggestion;
+import com.example.demo.service.SuggestionService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/suggestions")
+public class SuggestionController {
+
+    private final SuggestionService suggestionService;
+
+    public SuggestionController(SuggestionService suggestionService) {
+        this.suggestionService = suggestionService;
+    }
+
+    @GetMapping("/{farmId}")
+    public Suggestion generate(@PathVariable Long farmId) {
+        return suggestionService.generateSuggestion(farmId);
+    }
+}
