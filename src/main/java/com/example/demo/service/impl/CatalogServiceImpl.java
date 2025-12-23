f34747960d271b54afc78a2f3ee665d56b438bb4
bala-amypo <bala@amypo.in>
2025-12-23 04:22:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Crop;
+import com.example.demo.entity.Fertilizer;
+import com.example.demo.repository.CropRepository;
+import com.example.demo.repository.FertilizerRepository;
+import com.example.demo.service.CatalogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CatalogServiceImpl implements CatalogService {
+
+    private final CropRepository cropRepository;
+    private final FertilizerRepository fertilizerRepository;
+
+    public CatalogServiceImpl(CropRepository cropRepository,
+                              FertilizerRepository fertilizerRepository) {
+        this.cropRepository = cropRepository;
+        this.fertilizerRepository = fertilizerRepository;
+    }
+
+    @Override
+    public Crop addCrop(Crop crop) {
+        return cropRepository.save(crop);
+    }
+
+    @Override
+    public Fertilizer addFertilizer(Fertilizer fertilizer) {
+        return fertilizerRepository.save(fertilizer);
+    }
+
+    @Override
+    public List<Crop> getAllCrops() {
+        return cropRepository.findAll();
+    }
+
+    @Override
+    public List<Fertilizer> getAllFertilizers() {
+        return fertilizerRepository.findAll();
+    }
+}
