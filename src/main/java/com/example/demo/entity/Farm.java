e7fdee37b2f6d82e52ff678d17c4532c19da8921
bala-amypo <bala@amypo.in>
2025-12-26 18:53:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Farm.java
+++ src/main/java/com/example/demo/entity/Farm.java
@@ -4,21 +4,23 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "farms")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Farm {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
     private Double soilPH;
     private Double waterLevel;
     private String season;
-    
+
     @ManyToOne
-    @JoinColumn(name = "owner_id")
+    @JoinColumn(name = "user_id")
     private User owner;
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,18 +6,20 @@ import lombok.*;
 @Entity
 @Table(name = "users")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(unique = true)
-    private String email;
-    
+
     private String name;
+
+    private String email;
+
     private String password;
+
     private String role;
 }
